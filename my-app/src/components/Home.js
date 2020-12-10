4028deade39024933c43adaa512fb85228e1f818
morgan-jaouni <morgan.jaouni@ttu.edu>
2020-12-10 01:31:28 -0600

    Adds current container

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -5,16 +5,15 @@ function Home() {
     return (
       <div>
         <div class="homesection">
-        <h3>Hello, I'm</h3>
-        <h1>Morgan Jaouni</h1>
-        <h3>Full-Stack Software Engineer</h3>
-        <p>I have a passion for creating full-stack applications with meticulous wireframe/ERD planning, back-end configuration & front-end connection utilizing Node/Django frameworks.</p>
+                <h3>Hello, I'm</h3>
+            <h1>Morgan Jaouni</h1>
+                <h3>Full-Stack Software Engineer</h3>
+                 <p>I have a passion for creating full-stack applications with meticulous wireframe/ERD planning, back-end configuration & front-end connection utilizing Node/Django frameworks.</p>
         <footer>
-        <h3>Contact Me</h3>
-        <p>Morgan.jaouni@ttu.edu</p>
-        <p>512-820-5425</p>
+            <h3 class="contact">Contact</h3>
+                <p>Morgan.jaouni@ttu.edu</p>
+                <p>512-820-5425</p>
         </footer>
-
         </div>
       </div>
     );
--- my-app/src/components/Projects.js
+++ my-app/src/components/Projects.js
@@ -1,9 +1,16 @@
 import React from "react";
+import "../App.css";
 
 function Projects() {
     return (
       <div className="App">
-        <h1>Projects</h1>
+          <div class="projects">
+            <h3 class="current-header">Current v1.0</h3>
+            <p>Banking web application built with MongoDB, Express, Node</p>
+           <a class="anchor-image" href="http://rocky-anchorage-99030.herokuapp.com">
+            <img class="current" src="./Current.png" alt=""></img>
+            </a>
+        </div>
       </div>
     );
   }
