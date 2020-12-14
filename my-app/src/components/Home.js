8d1ed3db57714a0654fc592960c9705343879007
morgan-jaouni <morgan314@gmail.com>
2020-12-14 00:44:19 -0600

    Changes img src

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -10,10 +10,10 @@ function Home() {
                 <h2>Full-Stack Software Engineer</h2>
 
         <div class="fling-minislide">
-        <img src="./Football.png" alt="Slide 1" />
-        <img src="./Chemistry.png" alt="Slide 2" />
-        <img src="./Snow.png" alt="Slide 3" />
-        <img src="./Brother.png" alt="Slide 4" />
+        <img src="https://github.com/morgan-jaouni/ReactPortfolio/blob/main/my-app/public/Football.png?raw=true" alt="Slide 1" />
+        <img src="https://github.com/morgan-jaouni/ReactPortfolio/blob/main/my-app/public/Chemistry.png?raw=true" alt="Slide 2" />
+        <img src="https://github.com/morgan-jaouni/ReactPortfolio/blob/main/my-app/public/Snow.png?raw=true" alt="Slide 3" />
+        <img src="https://github.com/morgan-jaouni/ReactPortfolio/blob/main/my-app/public/Brother.png?raw=true" alt="Slide 4" />
           </div>
         </div>
         <footer>
