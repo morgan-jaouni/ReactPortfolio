6621ce595883bb1c2af4df917f824a7708caae24
morgan-jaouni <morgan.jaouni@ttu.edu>
2020-12-14 00:44:19 -0600

    Changes img src

--- my-app/src/components/Projects.js
+++ my-app/src/components/Projects.js
@@ -8,14 +8,14 @@ function Projects() {
             <h3 class="current-header">Current v1.0</h3>
             <p>Banking web application built with MongoDB, Express, Node</p>
              <a class="anchor-image" href="http://rocky-anchorage-99030.herokuapp.com">
-            <img class="current" src="./Current.png" alt=""></img>
+            <img class="current" src="https://github.com/morgan-jaouni/ReactPortfolio/blob/main/my-app/public/Current.png?raw=true" alt=""></img>
             </a>
             </div>
             <div class="blackjack-container">
             <h3 class="current-header">BlackJack</h3>
             <p>React.js blackjack game</p>
              <a class="blackjack-image" href="https://morgan-jaouni.github.io/Black-jack/">
-            <img class="blackjack-img" src="./Blackjack.png" alt=""></img>
+            <img class="blackjack-img" src="https://github.com/morgan-jaouni/ReactPortfolio/blob/main/my-app/public/Blackjack.png?raw=true" alt=""></img>
             </a>
         </div>
       </div>
