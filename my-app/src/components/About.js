c662682ed3742f191809964cae9cb259489130f0
morgan-jaouni <morgan314@gmail.com>
2021-01-07 18:04:17 -0600

    Ready for deployment

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -6,7 +6,6 @@ function About() {
     return (
       <div>
         <div>
-       
         <div class="fling-minislide">
         <img src="https://github.com/morgan-jaouni/ReactPortfolio/blob/main/my-app/public/Football.png?raw=true" alt="Slide 1" />
         <img src="https://github.com/morgan-jaouni/ReactPortfolio/blob/main/my-app/public/Chemistry.png?raw=true" alt="Slide 2" />
@@ -14,14 +13,6 @@ function About() {
         <img src="https://github.com/morgan-jaouni/ReactPortfolio/blob/main/my-app/public/Brother.png?raw=true" alt="Slide 4" />
           </div>
         </div>
-        <footer>
-            <h3 class="contact">Contact</h3>
-                <p class="email">Morgan.jaouni@ttu.edu</p>
-                <div>
-                <a class="social" href="https://www.linkedin.com/in/morgan-jaouni/">< FaLinkedinIn /></a>
-                <a class="social" href="https://github.com/morgan-jaouni">< FaGit /></a>
-                </div>
-        </footer>
       </div>
     );
   }
