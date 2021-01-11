80806f5270ed1e5d68ab646960a1dce64f1885f0
morgan-jaouni <morgan314@gmail.com>
2021-01-11 16:03:09 -0600

    Edits about page

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -11,6 +11,12 @@ function About() {
         <img src="https://github.com/morgan-jaouni/ReactPortfolio/blob/main/my-app/public/Snow.png?raw=true" alt="Slide 3" />
         <img src="https://github.com/morgan-jaouni/ReactPortfolio/blob/main/my-app/public/Brother.png?raw=true" alt="Slide 4" />
           </div>
+          <div class="about-container">
+            <h3 class="about-header">About</h3>
+              <div class="about-p">
+            <p class="about-me">I love Software, Science, & Sports! When I'm not coding, I can be found on the lake, in the gym, at the beach, or in the mountains. I am currently seeking collaborative Software Development roles!  </p>
+               </div>
+        </div>
         </div>
       </div>
     );
