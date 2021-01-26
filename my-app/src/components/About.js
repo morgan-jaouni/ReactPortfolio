fe8a1afec6766b3e5cd2925eb7728fdc7df9053c
morgan-jaouni <morgan314@gmail.com>
2021-01-26 12:11:22 -0600

    deletes about page

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -1,26 +1,26 @@
-import React from "react";
-import "../App.css";
+// import React from "react";
+// import "../App.css";
 
-function About() {
-    return (
-      <div>
-        <div>
-        <div class="fling-minislide">
-        <img src="https://github.com/morgan-jaouni/ReactPortfolio/blob/main/my-app/public/Football.png?raw=true" alt="Slide 1" />
-        <img src="https://github.com/morgan-jaouni/ReactPortfolio/blob/main/my-app/public/Brother.png?raw=true" alt="Slide 4" />
-        <img src="https://github.com/morgan-jaouni/ReactPortfolio/blob/main/my-app/public/Snow.png?raw=true" alt="Slide 3" />
-        <img src="https://github.com/morgan-jaouni/ReactPortfolio/blob/main/my-app/public/Chemistry.png?raw=true" alt="Slide 2" />
-          </div>
-          <div class="about-container">
-            <h3 class="about-header">About</h3>
-              <div class="about-p">
-            <p class="about-me">I love software, science, & sports! When I'm not coding, I can be found on the lake, in the gym, at the beach, or in the mountains. I am currently seeking collaborative Software Development roles!  </p>
-               </div>
-        </div>
-        </div>
-      </div>
-    );
-  }
+// function About() {
+//     return (
+//       <div>
+//         <div>
+//         <div class="fling-minislide">
+//         <img src="https://github.com/morgan-jaouni/ReactPortfolio/blob/main/my-app/public/Football.png?raw=true" alt="Slide 1" />
+//         <img src="https://github.com/morgan-jaouni/ReactPortfolio/blob/main/my-app/public/Brother.png?raw=true" alt="Slide 4" />
+//         <img src="https://github.com/morgan-jaouni/ReactPortfolio/blob/main/my-app/public/Snow.png?raw=true" alt="Slide 3" />
+//         <img src="https://github.com/morgan-jaouni/ReactPortfolio/blob/main/my-app/public/Chemistry.png?raw=true" alt="Slide 2" />
+//           </div>
+//           <div class="about-container">
+//             <h3 class="about-header">About</h3>
+//               <div class="about-p">
+//             <p class="about-me">I love software, science, & sports! When I'm not coding, I can be found on the lake, in the gym, at the beach, or in the mountains. I am currently seeking collaborative Software Development roles!  </p>
+//                </div>
+//         </div>
+//         </div>
+//       </div>
+//     );
+//   }
 
 
-  export default About;
+//   export default About;
--- my-app/src/components/Index.js
+++ my-app/src/components/Index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Home from "./Home";
 import Projects from "./Projects";
-import About from "./About";
 import { Switch,Route } from "react-router-dom";
 
 function Index() {
@@ -9,7 +8,6 @@ function Index() {
         <Switch>
             <Route exact path="/ReactPortfolio/Projects" component={Projects} />
             <Route exact path="/ReactPortfolio" component={Home} />
-            <Route exact path="/ReactPortfolio/About" component={About} />
         </Switch>
     );
   }
