2167ff8041b1c98b41b46c531e2ae6e12907a205
morgan-jaouni <morgan314@gmail.com>
2021-02-04 22:36:43 -0600

    Adds believer to proejcts

--- my-app/src/components/Projects.js
+++ my-app/src/components/Projects.js
@@ -26,20 +26,6 @@ function Projects() {
                </div>
             </div>
             <div class="projects-container">
-            <h3 class="projects-header">BlackJack</h3>
-             <a class="projects-image" href="https://morgan-jaouni.github.io/Black-jack/">
-            <img class="projects-img-anchor" src="https://github.com/morgan-jaouni/ReactPortfolio/blob/main/my-app/public/Blackjack.png?raw=true" alt=""></img>
-            </a>
-            <button class="projects-button">
-                <a class="current-git" href="https://github.com/morgan-jaouni/Black-jack">< FaGit /></a>
-              </button>
-              <div class="projects-border"></div>
-              <div class="technologies">
-            <p class="p-tech">Technologies:</p>
-            <p class="p-tech">React</p>
-               </div>
-        </div>
-            <div class="projects-container">
             <h3 class="projects-header">Wayfarer</h3>
              <a class="projects-image" href="https://anywhere-else-app.herokuapp.com">
             <img class="projects-img-anchor" src="https://github.com/morgan-jaouni/ReactPortfolio/blob/main/my-app/public/Wayfarer.png?raw=true" alt=""></img>
@@ -53,6 +39,38 @@ function Projects() {
             <p class="p-tech">Python</p>
             <p class="p-tech">Django</p>
             <p class="p-tech">PostgreSQL</p>
+               </div>
+        </div>
+            <div class="projects-container">
+            <h3 class="projects-header">Believer</h3>
+             <a class="projects-image" href="https://anywhere-else-app.herokuapp.com">
+            <img class="projects-img-anchor" src="https://github.com/morgan-jaouni/ReactPortfolio/blob/main/my-app/public/Wayfarer.png?raw=true" alt=""></img>
+            </a>
+            <button class="projects-button">
+                <a class="current-git" href="https://github.com/morgan-jaouni/Wayfarer">< FaGit /></a>
+              </button>
+              <div class="projects-border"></div>
+              <div class="technologies">
+            <p class="p-tech">Technologies:</p>
+            <p class="p-tech">MongoDB</p>
+            <p class="p-tech">Express</p>
+            <p class="p-tech">React</p>
+            <p class="p-tech">Node</p>
+               </div>
+        </div>
+            <div class="projects-container">
+            <h3 class="projects-header">BlackJack</h3>
+             <a class="projects-image" href="https://morgan-jaouni.github.io/Black-jack/">
+            <img class="projects-img-anchor" src="https://github.com/morgan-jaouni/ReactPortfolio/blob/main/my-app/public/Blackjack.png?raw=true" alt=""></img>
+            </a>
+            <button class="projects-button">
+                <a class="current-git" href="https://github.com/morgan-jaouni/Black-jack">< FaGit /></a>
+              </button>
+              <div class="projects-border"></div>
+              <div class="technologies">
+            <p class="p-tech">Technologies:</p>
+            <p class="p-tech">React</p>
+            <p class="p-tech">Vanilla JavaScript</p>
                </div>
         </div>
       </div>
