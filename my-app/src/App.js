412012b8c5fc8f308b8e16c55c30bd7243041ed7
morgan-jaouni <morgan314@gmail.com>
2021-02-24 21:10:10 -0600

    CNAME

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <div className="App">
       <nav class="navbar">
-      <Link class="navlinks" to="/ReactPortfolio">Home</Link>
-      <Link class="navlinks" to="/ReactPortfolio/Projects">Projects</Link>
+      <Link class="navlinks" to="/">Home</Link>
+      <Link class="navlinks" to="/Projects">Projects</Link>
       </nav>
       <Index></Index>
     </div>
