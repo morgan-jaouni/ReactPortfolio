482f2e59e3e57c28bc37f77097dd4ae3da0fa407
morgan-jaouni <morgan314@gmail.com>
2021-02-27 15:38:49 -0600

    adds styles folder

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "./App.css";
+import "./styles/projects.css";
+import "./styles/home.css";
+import "./styles/about.css";
+import "./styles/nav.css";
 import { Link } from 'react-router-dom';
 import Index from "./components/Index";
-import Projects from "./components/Projects";
 
 function App() {
   return (
@@ -10,6 +13,7 @@ function App() {
       <nav class="navbar">
       <Link class="navlinks" to="/">Home</Link>
       <Link class="navlinks" to="/Projects">Projects</Link>
+      <Link class="navlinks" to="/About">About</Link>
       </nav>
       <Index>
       </Index>
--- my-app/src/components/Index.js
+++ my-app/src/components/Index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Home from "./Home";
 import Projects from "./Projects";
+import About from "./About";
 import { Switch,Route } from "react-router-dom";
 
 function Index() {
     return (
         <Switch>
+            <Route exact path="/About" component={About} />
             <Route exact path="/Projects" component={Projects} />
             <Route exact path="/" component={Home} />
         </Switch>
