7b55cfc6ebedf14c8c8ac9b31a42f14061d6516b
morgan-jaouni <morgan314@gmail.com>
2021-02-28 17:35:07 -0600

    About page

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../App.css";
 
+
 function About() {
     return (
       <div>
@@ -8,24 +9,29 @@ function About() {
         <div class="about-div">
             <h1 class="about-header">About Me</h1>
             <div class="home-border"></div>
-            <p class="about-me">I'm a full-stack Software Engineer from Austin, Texas! <br></br> When I'm not coding, I enjoy spending time on the lake, at the gym, or in the mountains. I am currently seeking collaborative Software Development roles! <br></br> Contact: Morgan-jaouni@outlook.com</p>
+            <p class="about-me">I'm a full-stack Software Engineer from Austin, Texas! <br></br> When I'm not coding, I enjoy spending time on the lake, at the gym, or in the mountains. I am currently seeking collaborative Software Development roles! <br></br></p>
             <p class="skills">Skills</p>
             <div>
             </div>
             <p class="skills">Programming Languages</p>
             <ul>
-                <li class="skills">JavaScript</li>
-                <li class="skills">Python</li>
-                <li class="skills">HTML</li>
-                <li class="skills">CSS</li>
+                <li class="skills-list">JavaScript</li>
+                <li class="skills-list">Python</li>
+                <li class="skills-list">HTML</li>
+                <li class="skills-list">CSS</li>
             </ul>
             <p class="skills">Frameworks & Technologies</p>
             <ul>
-                <li class="skills">Node.js</li>
-                <li class="skills">React.js</li>
-                <li class="skills">MongoDb</li>
-                <li class="skills">CSS</li>
+                <li class="skills-list">Node.js</li>
+                <li class="skills-list">React.js</li>
+                <li class="skills-list">Axios</li>
+                <li class="skills-list">MongoDb</li>
+                <li class="skills-list">PostgreSQL</li>
+                <li class="skills-list">Django</li>
+                <li class="skills-list">Bootstrap</li>
             </ul>
+            <a class="resume" href="file:///Users/Morganjaouni/Downloads/Morgan-Jaouni_5%20(2).pdf">Resume</a>
+            <p> Contact: Morgan-jaouni@outlook.com</p>
         </div>
         </div>
         </div>
